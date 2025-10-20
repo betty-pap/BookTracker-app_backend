7b8655c94e5545a12e8bfa187cabb259e76e9f39
Betty Pap <130661020+betty-pap@users.noreply.github.com>
2025-10-20 23:54:38 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+
+const app = express();
+app.use(cors({
+  origin: ['http://localhost:8081', 'exp://localhost:19000', 'http://localhost:3000'], // Expo URLs
+  credentials: true
+}));
+
+//from env file
+const uri = process.env.MONGO_URI;
+const port = process.env.PORT || 3000;
+
+// Middleware 
+app.use(express.json());
+
+// Import routes
+const bookRoute = require("./routes/bookRoutes");
+//const authRoutes = require("./routes/auth");
+
+// Use routes
+app.use("/api/books", bookRoute);
+//app.use("/api/auth", authRoutes);
+
+//DB connection
+mongoose.connect(uri)
+  .then(() => console.log('connected'))
+  .catch(err => console.log(err));
+
+app.listen(port, () => {
+  console.log('backend running on port 3000 now');
+})
+
+
+
+
+
