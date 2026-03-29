04c41d5c22f7bdcaadac635261b44cad879a217e
Betty Pap <130661020+betty-pap@users.noreply.github.com>
2026-03-29 21:01:40 +0300

    shelf status fix

--- models/Book.js
+++ models/Book.js
@@ -35,16 +35,6 @@ const bookSchema = new mongoose.Schema({
     type: Date,
     default: Date.now
   },
-  // readingSessions: [{
-  //   date: {
-  //     type: Date,
-  //     default: Date.now
-  //   },
-  //   pagesRead: Number,
-  //   startPage: Number,
-  //   endPage: Number,
-  //   duration: Number // in minutes
-  // }],
   cover: {
     type: String,
     default: "https://placehold.co/100x150?text=No+Cover"
